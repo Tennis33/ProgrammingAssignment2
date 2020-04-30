c077bbe76dd35600252048948bb600cf7647d50a
Tennis33 <tenz_azngrl@hotmail.com>
2020-05-01 09:24:04 +1000

    lexical scoping assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##
 makeCacheMatrix <- function(x = matrix()) {
+  ## x is a square invertible matrix
+  ## following list contains functions used as input to cacheSolve()  
+  ##              1. set the matrix
+  ##              2. get the matrix
+  ##              3. set the inverse
+  ##              4. get the inverse
 
+  inv = NULL
+  set = function(y) {
+    # operator `<<-` is used to assign a value to an object in an environment i.e. different from the current environment. 
+    x <<- y
+    inv <<- NULL
+  }
+  get = function() x
+  setinv = function(inverse) inv <<- inverse 
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## x is the output of makeCacheMatrix()
+  ## returns inverse of the original matrix input to makeCacheMatrix()
+  
+  inv = x$getinv()
+  
+  # if the inverse has already been calculated
+  if (!is.null(inv)){
+    # this allows to obtain it from the cache and skips computation. 
+    message("getting cached data")
+    return(inv)
+  }
+  
+  # otherwise, calculates the inverse 
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+  
+  # use setinv function to set the value of the inverse in the cache.
+  x$setinv(inv)
+  
+  return(inv)
 }
